5c1ff6db6ae9f533939ed246e54e6defe1ce175c
dannykrug <dannykrug@gmail.com>
2018-12-30 15:47:11 -0500

    shoecell is linked

--- src/components/ShoeBox.js
+++ src/components/ShoeBox.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import ShoeCell from './ShoeCell'
 
 export default class ShoeBox extends Component {
   render(){
     return(
       <div>
       <h3>The Shoes You Own Go Here</h3>
+      <ShoeCell/>
       </div>
     )
   }
---
+++ src/components/ShoeCell.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class ShoeCell extends Component {
+  render(){
+    return(
+      <div>
+      <h4>Each Shoe Here</h4>
+      </div>
+    )
+  }
+}
