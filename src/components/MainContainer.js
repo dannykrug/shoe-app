c4d28aa861ff76cb87ae59168528f2d15f8d0951
dannykrug <dannykrug@gmail.com>
2018-12-30 15:44:35 -0500

    added shoebox

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import ShoeBox from './ShoeBox'
 
 export default class MainContainer extends Component {
   render() {
     return(
       <div>
       <h2>Shoes Go Here...</h2>
+      <ShoeBox/>
       </div>
     )
   }
---
+++ src/components/ShoeBox.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class ShoeBox extends Component {
+  render(){
+    return(
+      <div>
+      <h3>The Shoes You Own Go Here</h3>
+      </div>
+    )
+  }
+}
